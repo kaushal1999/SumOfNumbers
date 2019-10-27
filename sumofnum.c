a0f2201525230cdb258b2f88ce762aa4e56dd027
kaushal waghela <kaushalwaghela1999@gmail.com>
2019-10-27 23:11:18 +0530

    Update sumofnum.c

--- sumofnum.c
+++ sumofnum.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main()
 {
- int a,b,c;
- scanf("%d%d",&a,&b);
- c=a+b;
- printf("%d",c);
+ int a,b=0,i;
+printf("enter a positive nonzero number\n");
+ scanf("%d",&a);
+ for(i=1;i<=a;i==)
+ b=b+i;
+printf("required sum = %d",b);
  return 0;
 }
